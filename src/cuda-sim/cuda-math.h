6ed72dcffff9d7d617fa481080d46e5af4669a11
Andrea Picciau <a.picciau13@imperial.ac.uk>
2015-12-03 20:15:27 +0000

    Modify source code to compile with CUDA 3.0

--- src/cuda-sim/cuda-math.h
+++ src/cuda-sim/cuda-math.h
@@ -65,300 +65,299 @@
  */
 
 #ifndef CUDA_MATH
-#define CUDA_MATH
-
-// cuda math implementations
-#undef max
-#undef min
-namespace cuda_math {
-#define __attribute__(a) // to remove warnings inside math_functions.h
-#undef INT_MAX
-
-#if CUDART_VERSION < 3000
-// DEVICE_BUILTIN
-   struct int4 {
-      int x, y, z, w;
-   };
-   struct uint4 {
-      unsigned int x, y, z, w;
-   };
-   struct float4 {
-      float x, y, z, w;
-   };
-   struct float2 {
-      float x, y;
-   };
-    
-
-// DEVICE_BUILTIN
-   typedef struct int4 int4;
-   typedef struct uint4 uint4;
-   typedef struct float4 float4;
-   typedef struct float2 float2;
-
-extern float rsqrtf(float); // CUDA 2.3 beta
-
-#define CUDA_FLOAT_MATH_FUNCTIONS
-#include <device_types.h>
-#define __CUDA_INTERNAL_COMPILATION__
-#include <math_functions.h>
-#undef __CUDA_INTERNAL_COMPILATION__
-#undef __attribute__
-
-// float to integer conversion 
-int float2int(float a, enum cudaRoundMode mode)
-{
-   return __internal_float2uint(a, mode); 
-}
-
-// float to unsigned integer conversion 
-unsigned int float2uint(float a, enum cudaRoundMode mode)
-{
-   return __internal_float2uint(a, mode); 
-}
-
-float __ll2float_rz(long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TOWARDZERO); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ll2float_ru(long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_UPWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ll2float_rd(long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_DOWNWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-
-#else
-
-#define CUDA_FLOAT_MATH_FUNCTIONS
-#define __CUDACC__
-
-// implementing int to float intrinsics with different rounding modes 
-#include <device_types.h>
-#include <fenv.h>
-
-// 32-bit integer to float
-float __int2float_rn(int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TONEAREST); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __int2float_rz(int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TOWARDZERO); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __int2float_ru(int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_UPWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __int2float_rd(int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_DOWNWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-
-// 32-bit unsigned integer to float
-float __uint2float_rn(unsigned int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TONEAREST); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __uint2float_rz(unsigned int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TOWARDZERO); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __uint2float_ru(unsigned int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_UPWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __uint2float_rd(unsigned int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_DOWNWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-
-// 64-bit integer to float
-float __ll2float_rn(long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TONEAREST); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ll2float_rz(long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TOWARDZERO); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ll2float_ru(long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_UPWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ll2float_rd(long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_DOWNWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-
-// 64-bit unsigned integer to float 
-float __ull2float_rn(unsigned long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TONEAREST); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ull2float_rz(unsigned long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_TOWARDZERO); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ull2float_ru(unsigned long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_UPWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-float __ull2float_rd(unsigned long long int a) {
-   int orig_rnd_mode = fegetround();
-   fesetround(FE_DOWNWARD); 
-   float b = a;
-   fesetround(orig_rnd_mode); 
-   return b;
-}
-
-// float to integer conversion 
-int float2int(float a, enum cudaRoundMode mode)
-{
-   int tmp;
-   switch (mode) {
-   case cuda_math::cudaRoundZero: tmp = truncf(a);     break;
-   case cuda_math::cudaRoundNearest: tmp = nearbyintf(a); break;
-   case cuda_math::cudaRoundMinInf: tmp = floorf(a);     break;
-   case cuda_math::cudaRoundPosInf: tmp = ceilf(a);      break;
-   default: abort();
+   #define CUDA_MATH
+   
+   // cuda math implementations
+   #undef max
+   #undef min
+   namespace cuda_math {
+   #define __attribute__(a) // to remove warnings inside math_functions.h
+   #undef INT_MAX
+   
+   #if CUDART_VERSION <= 3000
+      // DEVICE_BUILTIN
+         struct int4 {
+            int x, y, z, w;
+         };
+         struct uint4 {
+            unsigned int x, y, z, w;
+         };
+         struct float4 {
+            float x, y, z, w;
+         };
+         struct float2 {
+            float x, y;
+         };
+          
+      
+      // DEVICE_BUILTIN
+         typedef struct int4 int4;
+         typedef struct uint4 uint4;
+         typedef struct float4 float4;
+         typedef struct float2 float2;
+      
+      extern float rsqrtf(float); // CUDA 2.3 beta
+      
+      #define CUDA_FLOAT_MATH_FUNCTIONS
+      #include <device_types.h>
+      #define __CUDA_INTERNAL_COMPILATION__
+      #include <common_functions.h>
+      #include <math_functions.h>
+      #undef __CUDA_INTERNAL_COMPILATION__
+      #undef __attribute__
+   
+      // float to integer conversion 
+      int float2int(float a, enum cudaRoundMode mode)
+      {
+         int tmp;
+         switch (mode) {
+         case cuda_math::cudaRoundZero: tmp = truncf(a);     break;
+         case cuda_math::cudaRoundNearest: tmp = nearbyintf(a); break;
+         case cuda_math::cudaRoundMinInf: tmp = floorf(a);     break;
+         case cuda_math::cudaRoundPosInf: tmp = ceilf(a);      break;
+         default: abort();
+         }
+         return tmp; 
+      }
+      
+      // float to unsigned integer conversion 
+      unsigned int float2uint(float a, enum cudaRoundMode mode)
+      {
+         unsigned int tmp;
+         switch (mode) {
+         case cuda_math::cudaRoundZero: tmp = truncf(a);     break;
+         case cuda_math::cudaRoundNearest: tmp = nearbyintf(a); break;
+         case cuda_math::cudaRoundMinInf: tmp = floorf(a);     break;
+         case cuda_math::cudaRoundPosInf: tmp = ceilf(a);      break;
+         default: abort();
+         }
+         return tmp; 
+      }
+      
+      float __internal_accurate_fdividef(float a, float b)
+      {
+         return fdividef(a, b); 
+      }
+   #else
+   
+      #define CUDA_FLOAT_MATH_FUNCTIONS
+      #define __CUDACC__
+      
+      // implementing int to float intrinsics with different rounding modes 
+      #include <device_types.h>
+      #include <fenv.h>
+      
+      // 32-bit integer to float
+      float __int2float_rn(int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TONEAREST); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __int2float_rz(int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TOWARDZERO); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __int2float_ru(int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_UPWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __int2float_rd(int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_DOWNWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      
+      // 32-bit unsigned integer to float
+      float __uint2float_rn(unsigned int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TONEAREST); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __uint2float_rz(unsigned int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TOWARDZERO); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __uint2float_ru(unsigned int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_UPWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __uint2float_rd(unsigned int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_DOWNWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      
+      // 64-bit integer to float
+      float __ll2float_rn(long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TONEAREST); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __ll2float_rz(long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TOWARDZERO); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __ll2float_ru(long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_UPWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __ll2float_rd(long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_DOWNWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      
+      // 64-bit unsigned integer to float 
+      float __ull2float_rn(unsigned long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TONEAREST); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __ull2float_rz(unsigned long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_TOWARDZERO); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __ull2float_ru(unsigned long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_UPWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      float __ull2float_rd(unsigned long long int a) {
+         int orig_rnd_mode = fegetround();
+         fesetround(FE_DOWNWARD); 
+         float b = a;
+         fesetround(orig_rnd_mode); 
+         return b;
+      }
+      
+      // float to integer conversion 
+      int float2int(float a, enum cudaRoundMode mode)
+      {
+         int tmp;
+         switch (mode) {
+         case cuda_math::cudaRoundZero: tmp = truncf(a);     break;
+         case cuda_math::cudaRoundNearest: tmp = nearbyintf(a); break;
+         case cuda_math::cudaRoundMinInf: tmp = floorf(a);     break;
+         case cuda_math::cudaRoundPosInf: tmp = ceilf(a);      break;
+         default: abort();
+         }
+         return tmp; 
+      }
+      
+      int __internal_float2int(float a, enum cudaRoundMode mode) 
+      {
+         return float2int(a, mode); 
+      }
+      
+      // float to unsigned integer conversion 
+      unsigned int float2uint(float a, enum cudaRoundMode mode)
+      {
+         unsigned int tmp;
+         switch (mode) {
+         case cuda_math::cudaRoundZero: tmp = truncf(a);     break;
+         case cuda_math::cudaRoundNearest: tmp = nearbyintf(a); break;
+         case cuda_math::cudaRoundMinInf: tmp = floorf(a);     break;
+         case cuda_math::cudaRoundPosInf: tmp = ceilf(a);      break;
+         default: abort();
+         }
+         return tmp; 
+      }
+      
+      unsigned int __internal_float2uint(float a, enum cudaRoundMode mode) 
+      {
+         return float2uint(a, mode); 
+      }
+      
+      // intrinsic for division 
+      float fdividef(float a, float b)
+      {
+         return (a / b); 
+      }
+      
+      float __internal_accurate_fdividef(float a, float b)
+      {
+         return fdividef(a, b); 
+      }
+      
+      // intrinsic for saturate  (clamp values beyond 0 and 1)
+      // float __saturatef(float a)
+      // {
+      //    float b; 
+      //    if (isnan(a)) b = 0.0f; 
+      //    else if (a >= 1.0f) b = 1.0f;
+      //    else if (a <= 0.0f) b = 0.0f; 
+      //    else b = a; 
+      //    return b; 
+      // }
+      
+      // intrinsic for power 
+      float __powf(float a, float b)
+      {
+         return powf(a, b);
+      }
+      
+      // math functions missing in Mac OSX GCC
+      #ifdef __APPLE__
+      int __signbitd(double d)
+      {
+         unsigned long long int u = *((unsigned long long int*)&d); 
+         return ((u & 0x8000000000000000ULL) != 0);
+      }
+      #endif 
+      
+      #undef __CUDACC__
+      #define __CUDA_INTERNAL_COMPILATION__
+      #include <math_functions.h>
+      #undef __CUDA_INTERNAL_COMPILATION__
+      #undef __attribute__
+   
+   #endif
+   
    }
-   return tmp; 
-}
-
-int __internal_float2int(float a, enum cudaRoundMode mode) 
-{
-   return float2int(a, mode); 
-}
-
-// float to unsigned integer conversion 
-unsigned int float2uint(float a, enum cudaRoundMode mode)
-{
-   unsigned int tmp;
-   switch (mode) {
-   case cuda_math::cudaRoundZero: tmp = truncf(a);     break;
-   case cuda_math::cudaRoundNearest: tmp = nearbyintf(a); break;
-   case cuda_math::cudaRoundMinInf: tmp = floorf(a);     break;
-   case cuda_math::cudaRoundPosInf: tmp = ceilf(a);      break;
-   default: abort();
+   
+   // math functions missing in Mac OSX GCC
+   #ifdef __APPLE__
+   int isnanf(float a) 
+   {
+      return (isnan(a)); 
    }
-   return tmp; 
-}
-
-unsigned int __internal_float2uint(float a, enum cudaRoundMode mode) 
-{
-   return float2uint(a, mode); 
-}
-
-// intrinsic for division 
-float fdividef(float a, float b)
-{
-   return (a / b); 
-}
-
-float __internal_accurate_fdividef(float a, float b)
-{
-   return fdividef(a, b); 
-}
-
-// intrinsic for saturate  (clamp values beyond 0 and 1)
-float __saturatef(float a)
-{
-   float b; 
-   if (isnan(a)) b = 0.0f; 
-   else if (a >= 1.0f) b = 1.0f;
-   else if (a <= 0.0f) b = 0.0f; 
-   else b = a; 
-   return b; 
-}
-
-// intrinsic for power 
-float __powf(float a, float b)
-{
-   return powf(a, b);
-}
-
-// math functions missing in Mac OSX GCC
-#ifdef __APPLE__
-int __signbitd(double d)
-{
-   unsigned long long int u = *((unsigned long long int*)&d); 
-   return ((u & 0x8000000000000000ULL) != 0);
-}
-#endif 
-
-#undef __CUDACC__
-#define __CUDA_INTERNAL_COMPILATION__
-#include <math_functions.h>
-#undef __CUDA_INTERNAL_COMPILATION__
-#undef __attribute__
-
-#endif
-
-}
-
-// math functions missing in Mac OSX GCC
-#ifdef __APPLE__
-int isnanf(float a) 
-{
-   return (isnan(a)); 
-}
-#endif 
+   #endif 
 
 #endif
